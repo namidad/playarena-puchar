6dec46601c417a85f8e7d2dd491663ec5ebcc055
Oliwier Salamon <225990@student.pwr.edu.pl>
2019-02-25 13:40:18 +0100

    Add files via upload

---
+++ build/precache-manifest.01ae2bf3680bf590473fb1b1693da573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a154226cd38847563dd",
+    "url": "/playarena-puchar/static/js/runtime~main.3a154226.js"
+  },
+  {
+    "revision": "496146305970e2d76ce0",
+    "url": "/playarena-puchar/static/js/main.49614630.chunk.js"
+  },
+  {
+    "revision": "b7aaead75d3d2888a362",
+    "url": "/playarena-puchar/static/js/2.b7aaead7.chunk.js"
+  },
+  {
+    "revision": "496146305970e2d76ce0",
+    "url": "/playarena-puchar/static/css/main.74ea9d8d.chunk.css"
+  },
+  {
+    "revision": "bd5e98331344a3ec378a1d59474dd3ff",
+    "url": "/playarena-puchar/index.html"
+  }
+];
