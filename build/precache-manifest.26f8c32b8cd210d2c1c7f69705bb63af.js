4893f751861eb4fb2490fe8efda4d8ed1b512682
Oliwier Salamon <225990@student.pwr.edu.pl>
2019-02-25 13:44:33 +0100

    Add files via upload

---
+++ build/precache-manifest.26f8c32b8cd210d2c1c7f69705bb63af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a154226cd38847563dd",
+    "url": "/playarena-puchar/static/js/runtime~main.3a154226.js"
+  },
+  {
+    "revision": "55a9c1ef25ccc33656b2",
+    "url": "/playarena-puchar/static/js/main.55a9c1ef.chunk.js"
+  },
+  {
+    "revision": "b7aaead75d3d2888a362",
+    "url": "/playarena-puchar/static/js/2.b7aaead7.chunk.js"
+  },
+  {
+    "revision": "55a9c1ef25ccc33656b2",
+    "url": "/playarena-puchar/static/css/main.74ea9d8d.chunk.css"
+  },
+  {
+    "revision": "fb9f2cced82d94715b2915b6c8490062",
+    "url": "/playarena-puchar/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends Component {
     return (
           <div>
             <Switch>
-              <Route exact path='https://namidad.github.io/playarena-puchar/' component={MainDraw}/>
-              <Route path='https://namidad.github.io/playarena-puchar/home' component={HomePage}/>
-              <Route path='https://namidad.github.io/playarena-puchar/create' component={CreatePools}/>
-              <Route path='https://namidad.github.io/playarena-puchar/wroclaw' component={MainDraw}/>
+              <Route exact path='/playarena-puchar/' component={MainDraw}/>
+              <Route path='/playarena-puchar/home' component={HomePage}/>
+              <Route path='/playarena-puchar/create' component={CreatePools}/>
+              <Route path='/playarena-puchar/wroclaw' component={MainDraw}/>
             </Switch>
           </div>
 
