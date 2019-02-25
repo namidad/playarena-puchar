c92bd679af29c1af07d158bffc51f4cfbd6508a5
Oliwier Salamon <salamonoliwier@gmail.com>
2019-02-25 13:42:39 +0100

    Updates

--- precache-manifest.26f8c32b8cd210d2c1c7f69705bb63af.js
+++ precache-manifest.26f8c32b8cd210d2c1c7f69705bb63af.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/playarena-puchar/static/js/runtime~main.3a154226.js"
   },
   {
-    "revision": "496146305970e2d76ce0",
-    "url": "/playarena-puchar/static/js/main.49614630.chunk.js"
+    "revision": "55a9c1ef25ccc33656b2",
+    "url": "/playarena-puchar/static/js/main.55a9c1ef.chunk.js"
   },
   {
     "revision": "b7aaead75d3d2888a362",
     "url": "/playarena-puchar/static/js/2.b7aaead7.chunk.js"
   },
   {
-    "revision": "496146305970e2d76ce0",
+    "revision": "55a9c1ef25ccc33656b2",
     "url": "/playarena-puchar/static/css/main.74ea9d8d.chunk.css"
   },
   {
-    "revision": "bd5e98331344a3ec378a1d59474dd3ff",
+    "revision": "fb9f2cced82d94715b2915b6c8490062",
     "url": "/playarena-puchar/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/playarena-puchar/precache-manifest.01ae2bf3680bf590473fb1b1693da573.js"
+  "/playarena-puchar/precache-manifest.26f8c32b8cd210d2c1c7f69705bb63af.js"
 );
 
 workbox.clientsClaim();
