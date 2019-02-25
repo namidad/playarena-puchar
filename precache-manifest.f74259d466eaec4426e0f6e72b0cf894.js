81ba8c06a57c9195016eb5c764e2cb916d08252f
Oliwier Salamon <salamonoliwier@gmail.com>
2019-02-25 13:08:40 +0100

    Updates

---
+++ precache-manifest.f74259d466eaec4426e0f6e72b0cf894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playarena-puchar/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3a154226cd38847563dd",
+    "url": "/playarena-puchar/static/js/runtime~main.3a154226.js"
+  },
+  {
+    "revision": "8f7552cbcc81bd667ac5",
+    "url": "/playarena-puchar/static/js/main.8f7552cb.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/playarena-puchar/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "8f7552cbcc81bd667ac5",
+    "url": "/playarena-puchar/static/css/main.b5ebfb57.chunk.css"
+  },
+  {
+    "revision": "0c239441dc8aa5f077688c60158a304a",
+    "url": "/playarena-puchar/index.html"
+  }
+];
